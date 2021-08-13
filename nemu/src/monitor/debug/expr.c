aa3bd8638db04714c4a40127240196ac1f350af4
tracer-ics2020 <tracer@njuics.org>
2021-08-13 16:52:00 +0800

    >  compile 191220000

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -4,6 +4,7 @@
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <regex.h>
+#include <stdlib.h>
 
 enum
 {
@@ -130,16 +131,212 @@ static bool make_token(char *e)
   return true;
 }
 
+static bool check_parentheses(int s, int e)
+{
+  if (tokens[s].type == '(' && tokens[e].type == ')')
+    return true;
+
+  return false;
+}
+
+static bool valid_parentheses(int s, int e)
+{
+  bool success = true;
+  //step 1: check lexical exception
+  for (int i = s; i <= e; i++)
+  {
+    if (i > s && (tokens[i].type == NUM || tokens[i].type == HEX || tokens[i].type == VAR || tokens[i].type == REG) && tokens[i - 1].type == ')')
+      return false;
+
+    if (i < e && (tokens[i].type == NUM || tokens[i].type == HEX || tokens[i].type == VAR || tokens[i].type == REG) && tokens[i + 1].type == '(')
+      return false;
+  }
+
+  //step 2: check valid parentheses
+  int length = 0;
+  for (int i = s; i <= e; ++i)
+  {
+    {
+      if (tokens[i].type == '(' || tokens[i].type == ')')
+        length++;
+    }
+  }
+
+  if (length % 2)
+    return false;
+  int stk[length + 1], top = 0;
+  for (int i = s; i <= e; i++)
+  {
+    if (tokens[i].type != ')' && tokens[i].type != '(')
+      continue;
+
+    char ch;
+    if (tokens[i].type == ')')
+      ch = '(';
+    else
+      ch = 0;
+
+    if (ch)
+    {
+      if (top == 0 || stk[top - 1] != ch)
+        return false;
+      top--;
+    }
+    else
+    {
+      stk[top++] = tokens[i].type;
+    }
+  }
+  if (top != 0)
+    return false;
+
+  return success;
+}
+
+static int dominant_op(int s, int e)
+{
+	int type_stack[e - s + 2];
+	int idx_stack[e - s + 2];
+
+	int top = 0;
+	for (int i = s; i <= e; i++)
+	{
+		char ch;
+		if (tokens[i].type == ')')
+			ch = '(';
+		else
+			ch = 0;
+
+		if (ch)
+		{
+			for (int j = top - 1; j >= 0; --j)
+			{
+				bool close = (type_stack[j] == '(');
+				type_stack[j] = 0;
+				idx_stack[j] = INT32_MIN;
+				top--;
+				if (close)
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			type_stack[top] = tokens[i].type;
+			idx_stack[top] = i;
+			top++;
+		}
+	}
+
+	for (int i = top - 1; i >= 0; --i)
+	{
+		if (type_stack[i] == '+' || type_stack[i] == '-')
+
+			return idx_stack[i];
+	}
+
+	for (int i = top - 1; i >= 0; --i)
+	{
+		if (type_stack[i] == '*' || type_stack[i] == '/')
+
+			return idx_stack[i];
+	}
+
+  for (int i = top - 1; i >= 0; --i)
+	{
+		if (type_stack[i] == TK_EQ)
+
+			return idx_stack[i];
+	}
+
+	return 0;
+}
+
+static word_t eval(int s, int e, bool *success)
+{
+	word_t val = 0;
+	if (s > e)
+	{
+		printf("eval ERROR: start position is greater than end\n");
+		*success = false;
+		return 0;
+	}
+	else if (s == e)
+	{
+		if (tokens[s].type == NUM){
+			val = atoi(tokens[s].str);
+		}
+		else if (tokens[s].type == VAR)
+		{
+			//val = get_varible(tokens[s].str, success);
+      printf("eval ERROR: Eval for VAR is not implemented.\n");
+      assert(0);
+		}
+    else if (tokens[s].type == REG)
+		{
+			//val = get_varible(tokens[s].str, success);
+      printf("eval ERROR: Eval for REG is not implemented.\n");
+      assert(0);
+		}
+
+		else if (tokens[s].type == HEX){
+			val = (word_t)strtoul(tokens[s].str, NULL, 0);
+
+		}
+	}
+	else
+	{
+		bool within_p = check_parentheses(s, e);
+
+		if (within_p == true)
+			val = eval(s + 1, e - 1, success);
+		else
+		{
+			int op = dominant_op(s, e);
+			// op = the position of dominant operator in the token expression;
+			word_t val1 = eval(s, op - 1, success);
+			word_t val2 = eval(op + 1, e, success);
+			// val2 = eval(op + 1, q);
+			switch (tokens[op].type)
+			{
+			case '+':
+				return val1 + val2;
+				break;
+			case '-':
+				return val1 - val2;
+				break;
+			case '*':
+				return val1 * val2;
+				break;
+			case '/':
+				return val1 / val2;
+      case TK_EQ:
+				return val1 == val2;
+				break;
+
+			default:
+				printf("eval ERROR: Eval for %d is not implemented.\n",tokens[op].type);
+				assert(0);
+			}
+		}
+	}
+	return val;
+}
+
+
 word_t expr(char *e, bool *success)
 {
-  if (!make_token(e))
+  if (!make_token(e) && valid_parentheses(0, nr_token - 1) )
   {
     *success = false;
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  word_t val=eval(0, nr_token - 1, success);
 
-  return 0;
+  // /* TODO: Insert codes to evaluate the expression. */
+  // TODO();
+
+  return val;
 }
