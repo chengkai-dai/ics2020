1898a140c583c3ca9782ec18e7109ae927a6b9a9
tracer-ics2020 <tracer@njuics.org>
2021-08-14 17:54:27 +0800

    >  compile 191220000

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -197,7 +197,7 @@ static inline def_rtl(auipc, rtlreg_t *dest, const sword_t imm)
 static inline def_rtl(jal, rtlreg_t *dest, const sword_t imm)
 {
   *dest = s->seq_pc;
-  rtl_j(s, imm + s->seq_pc - 4);
+  rtl_j(s, imm + s->seq_pc);
 }
 
 static inline def_rtl(jrelop, uint32_t relop,
