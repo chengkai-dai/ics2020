c8b89586c8bce7db568eebef0ebbc939ded03ffa
tracer-ics2020 <tracer@njuics.org>
2021-08-14 23:46:36 +0800

    >  compile 191220000

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -188,23 +188,6 @@ static inline def_rtl(jr, rtlreg_t *target)
   s->is_jmp = true;
 }
 
-static inline def_rtl(auipc, rtlreg_t *dest, const sword_t imm)
-{
-  *dest = imm + s->seq_pc - 4;
-}
-
-static inline def_rtl(jal, rtlreg_t *dest, const sword_t imm)
-{
-  *dest = s->seq_pc;
-  rtl_j(s, imm + s->seq_pc - 4);
-}
-
-static inline def_rtl(jalr, rtlreg_t *dest, const rtlreg_t *src1, const sword_t imm)
-{
-  vaddr_t target = (instr_sign_ext(imm) + *src1) & (~1);
-  *dest = s->seq_pc;
-  rtl_j(s, target);
-}
 
 static inline def_rtl(jrelop, uint32_t relop,
                       const rtlreg_t *src1, const rtlreg_t *src2, vaddr_t target)
--- nemu/src/isa/riscv64/local-include/rtl.h
+++ nemu/src/isa/riscv64/local-include/rtl.h
@@ -5,5 +5,22 @@
 #include "reg.h"
 
 // no isa-dependent rtl instructions
+static inline def_rtl(auipc, rtlreg_t *dest, const sword_t imm)
+{
+  *dest = imm + s->seq_pc - 4;
+}
+
+static inline def_rtl(jal, rtlreg_t *dest, const sword_t imm)
+{
+  *dest = s->seq_pc;
+  rtl_j(s, imm + s->seq_pc - 4);
+}
+
+static inline def_rtl(jalr, rtlreg_t *dest, const rtlreg_t *src1, const sword_t imm)
+{
+  vaddr_t target = (instr_sign_ext(imm) + *src1) & (~1);
+  *dest = s->seq_pc;
+  rtl_j(s, target);
+}
 
 #endif
