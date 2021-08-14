5825b328f6ebb15a568b7e163912e3c1c33b126e
tracer-ics2020 <tracer@njuics.org>
2021-08-14 15:22:20 +0800

    >  compile 191220000

--- nemu/src/isa/riscv64/exec/add.h
+++ nemu/src/isa/riscv64/exec/add.h
@@ -1,8 +1,8 @@
 static inline def_EHelper(add) {
     rtl_addi(s,ddest,dsrc1,id_src2->imm);
-    printf("id_src1->str %s\n",id_src1->str);
+    printf("id_dest->str %s\n",id_dest->str);
     printf("id_src2->imm %ld\n",id_src2->imm);
-    printf("reg_name(id_src1->reg) %s\n",reg_name(id_src1->reg));
+    printf("reg_name(id_dest->reg) %s\n",reg_name(id_dest->reg));
 
     print_Dop(id_src1->str, OP_STR_SIZE, "%ld(%s)", id_src2->imm, reg_name(id_src1->reg));
     switch (s->width) {
