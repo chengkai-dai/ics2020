83cb8cf9b0800c24607c1ebebb938e24cee8e17c
tracer-ics2020 <tracer@njuics.org>
2021-08-16 23:33:41 +0800

    >  run 191220000

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -3,43 +3,76 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
   return 0;
 }
 
-char *strcpy(char* dst,const char* src) {
+char *strcpy(char *dst, const char *src)
+{
   return NULL;
 }
 
-char* strncpy(char* dst, const char* src, size_t n) {
+char *strncpy(char *dst, const char *src, size_t n)
+{
   return NULL;
 }
 
-char* strcat(char* dst, const char* src) {
+char *strcat(char *dst, const char *src)
+{
   return NULL;
 }
 
-int strcmp(const char* s1, const char* s2) {
+int strcmp(const char *s1, const char *s2)
+{
+  int i = 0;
+  if (s1 == NULL && s2 == NULL)
+  {
+    return -1;
+  }
+
+  if (s1 == NULL && s2 != NULL)
+  {
+    return (int)s2[0];
+  }
+
+  if (s2 == NULL && s1 != NULL)
+  {
+    return (int)s1[0];
+  }
+
+  while (s1[i] != '\0' && s2[i] != '\0')
+  {
+    if (s1[i] != s2[i]){
+      return (int)(s1[i]-s2[i]);
+    }
+    i++;
+  }
+  return (int)(s1[i-1]-s2[i-1]);
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
   return 0;
 }
 
-int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
-}
-
-void* memset(void* v,int c,size_t n) {
+void *memset(void *v, int c, size_t n)
+{
   return NULL;
 }
 
-void* memmove(void* dst,const void* src,size_t n) {
+void *memmove(void *dst, const void *src, size_t n)
+{
   return NULL;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
+void *memcpy(void *out, const void *in, size_t n)
+{
   return NULL;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n) {
+int memcmp(const void *s1, const void *s2, size_t n)
+{
   return 0;
 }
 
