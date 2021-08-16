d0ec7e7f6b14ba41f0cf5779deeb601e8c14ef9b
tracer-ics2020 <tracer@njuics.org>
2021-08-16 18:12:30 +0800

    >  compile 191220000

--- nemu/src/isa/riscv64/local-include/decode.h
+++ nemu/src/isa/riscv64/local-include/decode.h
@@ -59,16 +59,23 @@ static inline def_DHelper(S)
 static inline def_DHelper(J)
 {
   decode_op_r(s, id_dest, s->isa.instr.j.rd, false);
-  sword_t simm = s->isa.instr.j.simm20 << 19 | s->isa.instr.j.simm19_12 << 11 | s->isa.instr.j.simm11 << 10 | s->isa.instr.j.simm10_1;
-  simm = simm << 1;
+  // simm = s->isa.instr.j.simm20 << 19 | s->isa.instr.j.simm19_12 << 11 | s->isa.instr.j.simm11 << 10 | s->isa.instr.j.simm10_1;
+  uint32_t imm = ((s->isa.instr.j.imm20 << 19) | (s->isa.instr.j.imm19_12 << 11) | (s->isa.instr.j.imm11 << 10) | (s->isa.instr.j.imm10_1)) << 1;
+  if (s->isa.instr.j.imm20)
+    imm = imm | (0xFFFFFFFF << 21);
+  sword_t simm = (sword_t)imm;
   decode_op_i(s, id_src1, simm, true);
+
+  // sword_t simm = s->isa.instr.j.simm20 << 19 | s->isa.instr.j.simm19_12 << 11 | s->isa.instr.j.simm11 << 10 | s->isa.instr.j.simm10_1;
+  // simm = simm << 1;
+  // decode_op_i(s, id_src1, simm, true);
 }
 
 static inline def_DHelper(B)
 {
   decode_op_r(s, id_src1, s->isa.instr.b.rs1, true);
-  uint32_t imm = ((s->isa.instr.b.simm12 << 11) | (s->isa.instr.b.simm11 << 10) | (s->isa.instr.b.simm10_5 << 4) | (s->isa.instr.b.simm4_1)) << 1;
-  if (s->isa.instr.b.simm12)
+  uint32_t imm = ((s->isa.instr.b.imm12 << 11) | (s->isa.instr.b.imm11 << 10) | (s->isa.instr.b.imm10_5 << 4) | (s->isa.instr.b.imm4_1)) << 1;
+  if (s->isa.instr.b.imm12)
     imm = imm | (0xFFFFFFFF << 13);
   sword_t simm = (sword_t)imm;
  
