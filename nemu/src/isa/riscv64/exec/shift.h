5aabc5d0384dff99e3928e55bb9e3beb5c350a81
tracer-ics2020 <tracer@njuics.org>
2021-08-16 18:43:50 +0800

    >  compile 191220000

--- nemu/src/isa/riscv64/exec/shift.h
+++ nemu/src/isa/riscv64/exec/shift.h
@@ -1,7 +1,12 @@
 static inline def_EHelper(shr)
 {
     if(id_src2->imm >> 10){
+        printf("before dest 0x%lx\n",*ddest);
+        printf("before dsrc1 0x%lx\n",*dsrc1);
+        printf("before imm %ld\n",id_src2->imm & 0x1F);
         rtl_sari(s,ddest, dsrc1, id_src2->imm);
+        printf("after dest 0x%lx\n",*ddest);
+
         print_asm_template3(srai);
     }
     else
