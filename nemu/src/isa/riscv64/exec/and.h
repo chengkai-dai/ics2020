320419be1987b90c32779bde3db825aff552ba94
tracer-ics2020 <tracer@njuics.org>
2021-08-16 19:03:59 +0800

    >  compile 191220000

--- nemu/src/isa/riscv64/exec/and.h
+++ nemu/src/isa/riscv64/exec/and.h
@@ -2,6 +2,13 @@ static inline def_EHelper(andi)
 {
     rtl_andi(s, ddest, dsrc1, id_src2->imm);
     print_asm_template3(andi);
+
+}
+
+static inline def_EHelper(andr)
+{
+    rtl_and(s, ddest, dsrc1, dsrc1);
+    print_asm_template3(andr);
     
 }
 
