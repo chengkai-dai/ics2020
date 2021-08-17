44a1b651609618f706681db0b7f8e4dd64926d1a
tracer-ics2020 <tracer@njuics.org>
2021-08-17 07:26:07 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 3b45033045ef4b44a76d41172851d2ac            Boot ID: 56c36ff3e265422686bc17afd357702a     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  07:26:07 up  1:24,  2 users,  load average: 0.08, 0.05, 0.06

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -17,14 +17,14 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
   uint8_t data7 = inb(RTC_ADDR + 6);
   uint8_t data8 = inb(RTC_ADDR + 7);
 
-  uint64_t time = ((uint64_t)data1) << 56 |
-                  ((uint64_t)data2) << 48 |
-                  ((uint64_t)data3) << 40 |
-                  ((uint64_t)data4) << 32 |
-                  ((uint64_t)data5) << 24 |
-                  ((uint64_t)data6) << 16 |
-                  ((uint64_t)data7) << 8 |
-                  data8;
+  uint64_t time = ((uint64_t)data5) << 56 |
+                  ((uint64_t)data6) << 48 |
+                  ((uint64_t)data7) << 40 |
+                  ((uint64_t)data8) << 32 |
+                  ((uint64_t)data1) << 24 |
+                  ((uint64_t)data2) << 16 |
+                  ((uint64_t)data3) << 8 |
+                  data4;
 
   // uint64_t time = ((uint64_t)data2) << 32 | data1;
 
