28601254f0bab57f65fc3902e56742905e2ed33e
tracer-ics2020 <tracer@njuics.org>
2021-08-17 02:55:45 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 5be86a1db1ff4f3689192f5873c00ae6            Boot ID: 48126a6e470a45cf84b8cafe8e92b7ca     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  02:55:45 up  2:56,  2 users,  load average: 0.13, 0.18, 0.17

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -8,7 +8,8 @@
 static uint8_t io_space[IO_SPACE_MAX] PG_ALIGN = {};
 static uint8_t *p_space = io_space;
 
-uint8_t* new_space(int size) {
+uint8_t *new_space(int size)
+{
   uint8_t *p = p_space;
   // page aligned;
   size = (size + (PAGE_SIZE - 1)) & ~PAGE_MASK;
@@ -17,17 +18,23 @@ uint8_t* new_space(int size) {
   return p;
 }
 
-static inline void check_bound(IOMap *map, paddr_t addr) {
+static inline void check_bound(IOMap *map, paddr_t addr)
+{
   Assert(map != NULL && addr <= map->high && addr >= map->low,
-      "address (0x%08x) is out of bound {%s} [0x%08x, 0x%08x] at pc = " FMT_WORD,
-      addr, (map ? map->name : "???"), (map ? map->low : 0), (map ? map->high : 0), cpu.pc);
+         "address (0x%08x) is out of bound {%s} [0x%08x, 0x%08x] at pc = " FMT_WORD,
+         addr, (map ? map->name : "???"), (map ? map->low : 0), (map ? map->high : 0), cpu.pc);
 }
 
-static inline void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_write) {
-  if (c != NULL) { c(offset, len, is_write); }
+static inline void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_write)
+{
+  if (c != NULL)
+  {
+    c(offset, len, is_write);
+  }
 }
 
-word_t map_read(paddr_t addr, int len, IOMap *map) {
+word_t map_read(paddr_t addr, int len, IOMap *map)
+{
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
@@ -37,7 +44,8 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   return data;
 }
 
-void map_write(paddr_t addr, word_t data, int len, IOMap *map) {
+void map_write(paddr_t addr, word_t data, int len, IOMap *map)
+{
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
