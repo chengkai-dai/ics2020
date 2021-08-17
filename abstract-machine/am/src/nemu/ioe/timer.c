5591ef7803f93def3ce1a3979ee662dd1f87e4b5
tracer-ics2020 <tracer@njuics.org>
2021-08-17 07:31:28 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 3b45033045ef4b44a76d41172851d2ac            Boot ID: 56c36ff3e265422686bc17afd357702a     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  07:31:28 up  1:30,  2 users,  load average: 0.18, 0.09, 0.07

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -9,31 +9,32 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
   printf("start\n");
   uint8_t data1 = inb(RTC_ADDR);
-  uint8_t data2 = inb(RTC_ADDR + 1);
-  uint8_t data3 = inb(RTC_ADDR + 2);
-  uint8_t data4 = inb(RTC_ADDR + 3);
-  uint8_t data5 = inb(RTC_ADDR + 4);
-  uint8_t data6 = inb(RTC_ADDR + 5);
-  uint8_t data7 = inb(RTC_ADDR + 6);
-  uint8_t data8 = inb(RTC_ADDR + 7);
+  printf("data1 %d\n",data1);
+//   uint8_t data2 = inb(RTC_ADDR + 1);
+//   uint8_t data3 = inb(RTC_ADDR + 2);
+//   uint8_t data4 = inb(RTC_ADDR + 3);
+//   uint8_t data5 = inb(RTC_ADDR + 4);
+//   uint8_t data6 = inb(RTC_ADDR + 5);
+//   uint8_t data7 = inb(RTC_ADDR + 6);
+//   uint8_t data8 = inb(RTC_ADDR + 7);
 
-  uint32_t sec = ((uint32_t)data5) << 24 |
-                 ((uint32_t)data6) << 16 |
-                 ((uint32_t)data7) << 8 |
-                 ((uint32_t)data8);
+//   uint32_t sec = ((uint32_t)data5) << 24 |
+//                  ((uint32_t)data6) << 16 |
+//                  ((uint32_t)data7) << 8 |
+//                  ((uint32_t)data8);
 
-  uint32_t usec = ((uint32_t)data1) << 24 |
-                  ((uint32_t)data2) << 16 |
-                  ((uint32_t)data3) << 8 |
-                  ((uint32_t)data4);
+//   uint32_t usec = ((uint32_t)data1) << 24 |
+//                   ((uint32_t)data2) << 16 |
+//                   ((uint32_t)data3) << 8 |
+//                   ((uint32_t)data4);
 
-  printf("sec %d\n",sec);
-printf("usec %d\n",usec);
-      // uint64_t time = ((uint64_t)data2) << 32 | data1;
-      // uint32_t data1 = inl(RTC_ADDR);
-      // uint8_t data2 = inl(RTC_ADDR + 4);
-      uint64_t time = ((uint64_t)sec) << 32 | usec;
-      uptime->us = time;
+//   printf("sec %d\n",sec);
+// printf("usec %d\n",usec);
+//       // uint64_t time = ((uint64_t)data2) << 32 | data1;
+//       // uint32_t data1 = inl(RTC_ADDR);
+//       // uint8_t data2 = inl(RTC_ADDR + 4);
+      // uint64_t time = ((uint64_t)sec) << 32 | usec;
+      uptime->us = 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc)
