a027faa98fc97915d174921cf71ea7683737ab07
tracer-ics2020 <tracer@njuics.org>
2021-08-17 07:32:16 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 3b45033045ef4b44a76d41172851d2ac            Boot ID: 56c36ff3e265422686bc17afd357702a     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  07:32:16 up  1:30,  2 users,  load average: 0.08, 0.07, 0.07

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -9,14 +9,23 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
   printf("start\n");
   uint8_t data1 = inb(RTC_ADDR);
-  printf("data1 %d\n",data1);
-//   uint8_t data2 = inb(RTC_ADDR + 1);
-//   uint8_t data3 = inb(RTC_ADDR + 2);
-//   uint8_t data4 = inb(RTC_ADDR + 3);
-//   uint8_t data5 = inb(RTC_ADDR + 4);
-//   uint8_t data6 = inb(RTC_ADDR + 5);
-//   uint8_t data7 = inb(RTC_ADDR + 6);
-//   uint8_t data8 = inb(RTC_ADDR + 7);
+  uint8_t data2 = inb(RTC_ADDR + 1);
+  uint8_t data3 = inb(RTC_ADDR + 2);
+  uint8_t data4 = inb(RTC_ADDR + 3);
+  uint8_t data5 = inb(RTC_ADDR + 4);
+  uint8_t data6 = inb(RTC_ADDR + 5);
+  uint8_t data7 = inb(RTC_ADDR + 6);
+  uint8_t data8 = inb(RTC_ADDR + 7);
+
+printf("data1 %d\n",data1);
+printf("data2 %d\n",data2);
+printf("data3 %d\n",data3);
+printf("data4 %d\n",data4);
+printf("data5 %d\n",data5);
+printf("data6 %d\n",data6);
+printf("data7 %d\n",data7);
+printf("data8 %d\n",data8);
+
 
 //   uint32_t sec = ((uint32_t)data5) << 24 |
 //                  ((uint32_t)data6) << 16 |
