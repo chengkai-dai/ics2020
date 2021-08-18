5f8f215b0292a89af569a235f5afdb7420c8b5a2
tracer-ics2020 <tracer@njuics.org>
2021-08-18 03:57:40 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 5be86a1db1ff4f3689192f5873c00ae6            Boot ID: 48126a6e470a45cf84b8cafe8e92b7ca     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  03:57:40 up 1 day,  3:58,  2 users,  load average: 0.30, 0.15, 0.11

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -6,11 +6,11 @@
 //#define MODE_800x600
 
 #ifdef MODE_800x600
-# define SCREEN_W 800
-# define SCREEN_H 600
+#define SCREEN_W 800
+#define SCREEN_H 600
 #else
-# define SCREEN_W 400
-# define SCREEN_H 300
+#define SCREEN_W 400
+#define SCREEN_H 300
 #endif
 #define SCREEN_SIZE ((SCREEN_H * SCREEN_W) * sizeof(uint32_t))
 
@@ -25,10 +25,11 @@
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 
-static uint32_t (*vmem) [SCREEN_W] = NULL;
+static uint32_t (*vmem)[SCREEN_W] = NULL;
 static uint32_t *vgactl_port_base = NULL;
 
-static inline void update_screen() {
+static inline void update_screen()
+{
 #ifdef SHOW_SCREEN
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(vmem[0][0]));
   SDL_RenderClear(renderer);
@@ -37,12 +38,19 @@ static inline void update_screen() {
 #endif
 }
 
-void vga_update_screen() {
+void vga_update_screen()
+{
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  if (vgactl_port_base[1] != 0)
+  {
+    update_screen();
+    vgactl_port_base[1] = 0;
+  }
 }
 
-void init_vga() {
+void init_vga()
+{
 #ifdef SHOW_SCREEN
   SDL_Window *window = NULL;
   char title[128];
@@ -55,7 +63,7 @@ void init_vga() {
 #endif
   SDL_SetWindowTitle(window, title);
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-      SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
+                              SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
 #endif
 
   vgactl_port_base = (void *)new_space(8);
@@ -66,4 +74,4 @@ void init_vga() {
   vmem = (void *)new_space(SCREEN_SIZE);
   add_mmio_map("vmem", VMEM, (void *)vmem, SCREEN_SIZE, NULL);
 }
-#endif	/* HAS_IOE */
+#endif /* HAS_IOE */
