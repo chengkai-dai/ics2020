6f2673bde595bacdaceee951f1a4e849b7f3b2d4
tracer-ics2020 <tracer@njuics.org>
2021-08-18 04:32:59 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 5be86a1db1ff4f3689192f5873c00ae6            Boot ID: 48126a6e470a45cf84b8cafe8e92b7ca     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  04:32:59 up 1 day,  4:33,  2 users,  load average: 0.04, 0.07, 0.09

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -5,14 +5,16 @@
 
 void __am_gpu_init()
 {
-  uint32_t size = inl(VGACTL_ADDR);
-  int i;
-  int w = size >> 16;; // TODO: get the correct width
-  int h = (int)(int16_t)size;; // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i++)
-    fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // uint32_t size = inl(VGACTL_ADDR);
+  // int i;
+  // int w = size >> 16;
+  // ; // TODO: get the correct width
+  // int h = (int)(int16_t)size;
+  // ; // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i++)
+  //   fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
@@ -24,11 +26,44 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
       .present = true, .has_accel = false, .width = w, .height = h, .vmemsz = 0};
 }
 
+//int x, y; void *pixels; int w, h; bool sync
+//AM帧缓冲控制器, 可写入绘图信息, 向屏幕(x, y)坐标处绘制w*h的矩形图像.
+//图像像素按行优先方式存储在pixels中, 每个像素用32位整数以00RRGGBB的方式描述颜色.
+//若sync为true, 则马上将帧缓冲中的内容同步到屏幕上
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 {
+
+  //assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+  // for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+  //   *p = 0;
+  // }
+
+  uint32_t size = inl(VGACTL_ADDR);
+  int width = size >> 16;
+  int height = (int)(int16_t)size;
+
   if (ctl->sync)
   {
     outl(SYNC_ADDR, 1);
+
+    uint32_t *pixel_info = (uint32_t *)(uintptr_t)FB_ADDR;
+    uint32_t *buffer = (uint32_t *)(uintptr_t)ctl->pixels;
+
+    printf("width %d\n",width);
+    printf("height %d\n",height);
+    printf("x %d\n",ctl->x);
+    printf("y %d\n",ctl->y);
+    printf("w %d\n",ctl->w);
+    printf("h %d\n",ctl->h);
+
+    for (int i = 0; i < ctl->h; ++i)
+    {
+      for (int j = 0; j < ctl->w; ++j)
+      {
+        // *(pixel_info + ctl->x + i * width + j) = inl(VGACTL_ADDR + 4 * (ctl->x + i * width + j));
+        pixel_info[ctl->y * width + ctl->x + i * width + j] = buffer[ctl->y * width + ctl->x + i * width + j];
+      }
+    }
   }
 }
 
