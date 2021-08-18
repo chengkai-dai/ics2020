b041e62783a62aea6d23aa03d1832c80435f63b2
tracer-ics2020 <tracer@njuics.org>
2021-08-18 21:05:24 +0800

    >  compile 191220000

--- nemu/include/isa/riscv64.h
+++ nemu/include/isa/riscv64.h
@@ -71,7 +71,7 @@ typedef struct
       uint32_t rs1 : 5;
       uint32_t rs2 : 5;
       uint32_t imm10_5 : 6;
-      int32_t simm12 : 1;
+      uint32_t imm12 : 1;
     } b;
     struct
     {
@@ -81,7 +81,7 @@ typedef struct
       uint32_t imm19_12 : 8;
       uint32_t imm11 : 1;
       uint32_t imm10_1 : 10;
-      int32_t simm20 : 1;
+      uint32_t imm20 : 1;
     } j;
     uint32_t val;
   } instr;
