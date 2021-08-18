992330ea0778b79aac68278a9944872b765cec3e
tracer-ics2020 <tracer@njuics.org>
2021-08-18 05:03:55 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 5be86a1db1ff4f3689192f5873c00ae6            Boot ID: 48126a6e470a45cf84b8cafe8e92b7ca     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  05:03:55 up 1 day,  5:04,  2 users,  load average: 0.73, 0.39, 0.36

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,45 +4,58 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+static char *addr = NULL;
 
-int rand(void) {
+int rand(void)
+{
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
+void srand(unsigned int seed)
+{
   next = seed;
 }
 
-int abs(int x) {
+int abs(int x)
+{
   return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr)
+{
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
+  while (*nptr == ' ')
+  {
+    nptr++;
+  }
+  while (*nptr >= '0' && *nptr <= '9')
+  {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
 
-void *malloc(size_t size) {
-  // size  = (size_t)ROUNDUP(size, 8);
-  // unsigned long int old = old_addr;
-  // printf("")
-  // old += size;
-  // assert((uintptr_t)heap.start <= (uintptr_t)old_addr && (uintptr_t)old_addr < (uintptr_t)heap.end);
-  // for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)old_addr; p ++) {
-  //   *p = 0;
-  // }
-  // return (void*)old;
-  return NULL;
+void *malloc(size_t size)
+{
+  if (addr == NULL)
+    addr = (void *)ROUNDUP(heap.start, 8);
+
+  size = (size_t)ROUNDUP(size, 8);
+  char *old = addr;
+  addr += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)addr && (uintptr_t)addr < (uintptr_t)heap.end);
+  for (char *p = (char *)old; p != (char *)addr; p++)
+  {
+    *p = 0;
+  }
+  return old;
 }
 
-void free(void *ptr) {
+void free(void *ptr)
+{
 }
 
 #endif
