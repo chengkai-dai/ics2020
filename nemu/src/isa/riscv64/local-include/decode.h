cd2411cf866f8c711bfce4395d5356af98164fbf
tracer-ics2020 <tracer@njuics.org>
2021-08-18 21:04:10 +0800

    >  compile 191220000

--- nemu/src/isa/riscv64/local-include/decode.h
+++ nemu/src/isa/riscv64/local-include/decode.h
@@ -60,11 +60,11 @@ static inline def_DHelper(J)
 {
   decode_op_r(s, id_dest, s->isa.instr.j.rd, false);
   // simm = s->isa.instr.j.simm20 << 19 | s->isa.instr.j.simm19_12 << 11 | s->isa.instr.j.simm11 << 10 | s->isa.instr.j.simm10_1;
-  uint32_t imm = ((s->isa.instr.j.imm20 << 19) | (s->isa.instr.j.imm19_12 << 11) | (s->isa.instr.j.imm11 << 10) | (s->isa.instr.j.imm10_1)) << 1;
-  if (s->isa.instr.j.imm20)
-    imm = imm | (0xFFFFFFFF << 21);
-  sword_t simm = (sword_t)(int32_t)imm;
-
+  // uint32_t imm = ((s->isa.instr.j.imm20 << 19) | (s->isa.instr.j.imm19_12 << 11) | (s->isa.instr.j.imm11 << 10) | (s->isa.instr.j.imm10_1)) << 1;
+  // if (s->isa.instr.j.imm20)
+  //   imm = imm | (0xFFFFFFFF << 21);
+  // sword_t simm = (sword_t)(int32_t)imm;
+  sword_t simm = ((s->isa.instr.j.simm20 << 19) | (s->isa.instr.j.imm19_12 << 11) | (s->isa.instr.j.imm11 << 10) | (s->isa.instr.j.imm10_1)) << 1;
   decode_op_i(s, id_src1, simm, true);
 
   // sword_t simm = s->isa.instr.j.simm20 << 19 | s->isa.instr.j.simm19_12 << 11 | s->isa.instr.j.simm11 << 10 | s->isa.instr.j.simm10_1;
@@ -75,11 +75,11 @@ static inline def_DHelper(J)
 static inline def_DHelper(B)
 {
   decode_op_r(s, id_src1, s->isa.instr.b.rs1, true);
-  uint32_t imm = ((s->isa.instr.b.imm12 << 11) | (s->isa.instr.b.imm11 << 10) | (s->isa.instr.b.imm10_5 << 4) | (s->isa.instr.b.imm4_1)) << 1;
-  if (s->isa.instr.b.imm12)
-    imm = imm | (0xFFFFFFFF << 13);
-  sword_t simm = (sword_t)(int32_t)imm;
- 
+  // uint32_t imm = ((s->isa.instr.b.imm12 << 11) | (s->isa.instr.b.imm11 << 10) | (s->isa.instr.b.imm10_5 << 4) | (s->isa.instr.b.imm4_1)) << 1;
+  // if (s->isa.instr.b.imm12)
+  //   imm = imm | (0xFFFFFFFF << 13);
+  // sword_t simm = (sword_t)(int32_t)imm;
+  sword_t simm = ((s->isa.instr.b.simm12 << 11) | (s->isa.instr.b.imm11 << 10) | (s->isa.instr.b.imm10_5 << 4) | (s->isa.instr.b.imm4_1)) << 1;
   decode_op_i(s, id_src2, simm, true);
   decode_op_r(s, id_dest, s->isa.instr.b.rs2, true);
 }
