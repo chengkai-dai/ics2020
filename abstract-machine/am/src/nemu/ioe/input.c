e87431776b0551f5c61af2b4b843b81f36c7c1c3
tracer-ics2020 <tracer@njuics.org>
2021-08-17 22:51:13 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 5be86a1db1ff4f3689192f5873c00ae6            Boot ID: 48126a6e470a45cf84b8cafe8e92b7ca     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  22:51:13 up 22:51,  2 users,  load average: 0.19, 0.19, 0.16

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -11,14 +11,14 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
   uint32_t keycode = inw(KBD_ADDR);
   printf("keycode %d\n",keycode);
 
-  if (keycode == AM_KEY_NONE)
+  if (keycode & KEYDOWN_MASK)
   {
-    kbd->keydown = false;
-    kbd->keycode = AM_KEY_NONE;
+    kbd->keydown = true;
+    kbd->keycode = (int)(uint8_t)keycode;
   }
   else
   {
-    kbd->keydown = true;
-    kbd->keycode = keycode | KEYDOWN_MASK;
+    kbd->keydown = false;
+    kbd->keycode = AM_KEY_NONE;
   }
 }
