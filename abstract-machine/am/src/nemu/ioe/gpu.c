b4407fc1710ebb1659a50f281a0108263f78b5fb
tracer-ics2020 <tracer@njuics.org>
2021-08-18 03:44:49 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 5be86a1db1ff4f3689192f5873c00ae6            Boot ID: 48126a6e470a45cf84b8cafe8e92b7ca     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  03:44:49 up 1 day,  3:45,  2 users,  load average: 0.25, 0.19, 0.12

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -1,25 +1,40 @@
 #include <am.h>
 #include <nemu.h>
-
+#include "klib.h"
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
+void __am_gpu_init()
+{
+  int i;
+  int w = 400; // TODO: get the correct width
+  int h = 300; // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
+{
+  uint32_t size = inw(VGACTL_ADDR);
+  int w = size >> 16;
+  int h = (int)(int16_t)size;
+
+  printf("width %d\n",w);
+  printf("height %d\n",h);
+  *cfg = (AM_GPU_CONFIG_T){
+      .present = true, .has_accel = false, .width = w, .height = h, .vmemsz = 0};
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
+{
+  if (ctl->sync)
+  {
     outl(SYNC_ADDR, 1);
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
+void __am_gpu_status(AM_GPU_STATUS_T *status)
+{
   status->ready = true;
 }
