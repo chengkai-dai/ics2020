a51031eea04845a8e0067eadf99974aedd0a65a7
tracer-ics2020 <tracer@njuics.org>
2021-08-19 04:53:56 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 3b91a374dab448a4ad1648e7a281a585     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  04:53:56 up  4:26,  2 users,  load average: 0.09, 0.07, 0.01

--- nemu/src/isa/riscv64/exec/system.h
+++ nemu/src/isa/riscv64/exec/system.h
@@ -10,6 +10,9 @@ static inline def_EHelper(ecall)
     //change mode from user to supervisor -- we can't see this
 
     s->seq_pc = cpu.csr[3]._64;
-    *(&cpu.csr[2]._64) = cpu.pc;
+    *(&cpu.csr[1]._64) = cpu.pc;
+    printf("epc 0x%lx",cpu.csr[1]._64);
+    printf("seq_pc 0x%lx",s->seq_pc);
+
     print_asm("ecall\n");
 }
