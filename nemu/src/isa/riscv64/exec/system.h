08e55c6be9206f399723f76d514a7350508e2527
tracer-ics2020 <tracer@njuics.org>
2021-08-19 04:56:22 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 3b91a374dab448a4ad1648e7a281a585     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  04:56:22 up  4:28,  2 users,  load average: 0.33, 0.12, 0.03

--- nemu/src/isa/riscv64/exec/system.h
+++ nemu/src/isa/riscv64/exec/system.h
@@ -11,8 +11,5 @@ static inline def_EHelper(ecall)
 
     *(&cpu.csr[1]._64) = s->seq_pc;
     s->seq_pc = cpu.csr[3]._64;
-    printf("epc 0x%lx\n",cpu.csr[1]._64);
-    printf("seq_pc 0x%lx\n",s->seq_pc);
-
     print_asm("ecall\n");
 }
