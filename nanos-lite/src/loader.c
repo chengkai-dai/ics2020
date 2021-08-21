2b5ce910226bf257c29506357c2c587f8907634a
tracer-ics2020 <tracer@njuics.org>
2021-08-21 00:21:34 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 3b91a374dab448a4ad1648e7a281a585     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  00:21:35 up 1 day, 23:54,  2 users,  load average: 0.00, 0.07, 0.03

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,22 +1,52 @@
 #include <proc.h>
 #include <elf.h>
 
+extern uint8_t ramdisk_start;
+
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  Elf64_Ehdr *elf;
+  Elf64_Phdr *ph, *eph;
+
+  elf = (void *)(&ramdisk_start);
+  Log("ELF loading from ram disk.");
+
+  assert(*(uint32_t *)elf->e_ident == 0x464c457f);
+
+  ph = (void *)(elf + elf->e_phoff);
+  eph = ph + elf->e_phnum;
+
+  for (; ph < eph; ++ph)
+  {
+    if (ph->p_type == PT_LOAD)
+    {
+      memcpy((void *)ph->p_vaddr, (void *)ph->p_offset, ph->p_memsz);
+
+      if (ph->p_memsz > ph->p_filesz)
+        memset((void *)ph->p_vaddr + ph->p_filesz, 0, (ph->p_memsz - ph->p_filesz));
+    }
+  }
+
+  // volatile uint32_t entry = elf->e_entry;
+
+  uint32_t entry = elf->e_entry;
+
+  Log("e_entry 0x%x\n", entry);
+
+  return entry;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename)
+{
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
-
