78dc2ef31188084ad90c5cb1fc98ba82f348030a
tracer-ics2020 <tracer@njuics.org>
2021-08-21 01:00:33 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 3b91a374dab448a4ad1648e7a281a585     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  01:00:33 up 2 days, 33 min,  2 users,  load average: 0.22, 0.14, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,19 +40,13 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     if (ph->p_type == PT_LOAD)
     {
       printf("PT_LOAD\n");
-
-      // void buf[1024];
       
-      // ramdisk_read(buf, (size_t *)ph->p_offset, ph->p_memsz);
+      ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_memsz);
 
-      // memcpy((void *)ph->p_vaddr, buf, ph->p_memsz);
-
-      // if (ph->p_memsz > ph->p_filesz)
-      //   memset((void *)ph->p_vaddr + ph->p_filesz, 0, (ph->p_memsz - ph->p_filesz));
+      if (ph->p_memsz > ph->p_filesz)
+        memset((void *)ph->p_vaddr + ph->p_filesz, 0, (ph->p_memsz - ph->p_filesz));
     }
-    else{
-      printf("PT_UNLOAD\n");
-    }
+    
   }
 
   // volatile uint32_t entry = elf->e_entry;
