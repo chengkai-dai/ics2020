9fd0c98bcd609581a482862e008fac5a721bdfee
tracer-ics2020 <tracer@njuics.org>
2021-08-23 02:07:20 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 092453fecdf44e94a8850cfee643c986     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  02:07:20 up  5:47,  2 users,  load average: 0.11, 0.13, 0.07

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,8 +62,10 @@ int fs_open(const char *pathname, int flags, int mode)
 {
   for (int i = 3; i < file_length; ++i)
   {
-    if (strcmp(pathname, file_table[i].name) == 0)
+    if (strcmp(pathname, file_table[i].name) == 0){
+      file_table[i].open_offset=0;
       return i;
+    }
   }
   assert(0);
   return -1;
