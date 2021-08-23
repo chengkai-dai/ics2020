ac8919badcebee4c1a9859d6d71fee49d9b0bf96
tracer-ics2020 <tracer@njuics.org>
2021-08-22 21:42:43 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 092453fecdf44e94a8850cfee643c986     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  21:42:44 up  1:23,  2 users,  load average: 0.09, 0.06, 0.01

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,11 @@
 #include <fs.h>
+#include <stdio.h>
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
-typedef struct {
+typedef struct
+{
   char *name;
   size_t size;
   size_t disk_offset;
@@ -11,53 +13,66 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum
+{
+  FD_STDIN,
+  FD_STDOUT,
+  FD_STDERR,
+  FD_FB
+};
 
-size_t invalid_read(void *buf, size_t offset, size_t len) {
+size_t invalid_read(void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
 
-size_t invalid_write(const void *buf, size_t offset, size_t len) {
+size_t invalid_write(const void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
-void init_fs() {
+void init_fs()
+{
   // TODO: initialize the size of /dev/fb
 }
 
-
-int fs_open(const char *pathname, int flags, int mode){
-
-  return 0;
-
+int fs_open(const char *pathname, int flags, int mode)
+{
+  int length = (int)(sizeof(file_table) / sizeof(file_table[0]));
+  for (int i = 3; i < length; ++i)
+  {
+    if (strcmp(pathname, file_table[i].name) == 0)
+      return i;
+  }
+  return -1;
 }
-size_t fs_read(int fd, void *buf, size_t len){
+size_t fs_read(int fd, void *buf, size_t len)
+{
 
   return 0;
-
 }
-size_t fs_write(int fd, const void *buf, size_t len){
+size_t fs_write(int fd, const void *buf, size_t len)
+{
 
   return 0;
-
 }
-size_t fs_lseek(int fd, size_t offset, int whence){
+size_t fs_lseek(int fd, size_t offset, int whence)
+{
 
   return 0;
-
 }
-int fs_close(int fd){
+int fs_close(int fd)
+{
 
   return 0;
-
 }
