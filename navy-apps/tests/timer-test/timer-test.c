716214d199855cc03980e31429c7b65f4431e27a
tracer-ics2020 <tracer@njuics.org>
2021-08-25 19:48:32 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 902f209ea12a412f902afffdff677e50     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  19:48:32 up  1:19,  2 users,  load average: 0.04, 0.06, 0.01

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,14 +1,28 @@
 #include <time.h>
 #include <stdio.h>
 
-static unsigned long get_time() {
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  unsigned int seconds = now.tv_sec;
-  unsigned int useconds = now.tv_usec;
-  return seconds * 1000 + (useconds + 500) / 1000;
+static unsigned long get_time()
+{
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    unsigned int seconds = now.tv_sec;
+    unsigned int useconds = now.tv_usec;
+    return seconds * 1000 + (useconds + 500) / 1000;
 }
 
-int main() {
-    unsigned long get_time()
+int main()
+{
+    unsigned long start_time = get_time();
+    unsigned long rec = start_time;
+    while (1)
+    {
+        unsigned long current_time = get_time();
+        if (current_time - start_time > 490 && current_time - start_time < 510)
+        {
+            printf("hello in %d ms\n", (int)(current_time - start_time));
+        }
+        start_time = current_time;
+    }
+
+    return 0;
 }
