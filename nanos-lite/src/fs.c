dcba02377c58b487b94d7acd4d510a44216f2847
tracer-ics2020 <tracer@njuics.org>
2021-08-25 23:18:26 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 902f209ea12a412f902afffdff677e50     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  23:18:26 up  4:49,  2 users,  load average: 0.68, 0.33, 0.27

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,6 +68,8 @@ void init_fs()
   // TODO: initialize the size of /dev/fb
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
+  printf("w %d\n",w);
+  printf("h %d\n",h);
   file_table[FD_FB].size = w * h * 4;
 }
 
