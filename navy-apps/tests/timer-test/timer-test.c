1ef3a833ac6ef60e3c7822f5c621c6399f3bb36f
tracer-ics2020 <tracer@njuics.org>
2021-08-25 20:42:13 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 902f209ea12a412f902afffdff677e50     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  20:42:14 up  2:13,  2 users,  load average: 0.09, 0.06, 0.05

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,28 +1,22 @@
-#include <time.h>
 #include <stdio.h>
-
-static unsigned long get_time()
-{
-    struct timeval now;
-    gettimeofday(&now, NULL);
-    unsigned int seconds = now.tv_sec;
-    unsigned int useconds = now.tv_usec;
-    return seconds * 1000 + (useconds + 500) / 1000;
-}
 
 int main()
 {
-    unsigned long start_time = get_time();
+        NDL_Init(0);
+
+    unsigned long start_time = NDL_GetTicks();
 
     while (1)
     {
-        unsigned long current_time = get_time();
+        unsigned long current_time = NDL_GetTicks();
 
         if ((current_time - start_time) % 500 < 7)
         {
             printf("hello in %d ms\n", (int)(current_time - start_time));
         }
     }
+
+
 
     return 0;
 }
