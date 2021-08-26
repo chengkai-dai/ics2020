1aa55f06d4ffb1209aed79fedd892d03144493ed
tracer-ics2020 <tracer@njuics.org>
2021-08-25 21:03:01 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 902f209ea12a412f902afffdff677e50     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  21:03:01 up  2:33,  2 users,  load average: 0.08, 0.05, 0.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -5,6 +5,7 @@ typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern size_t serial_write(const void *buf, size_t offset, size_t len);
+extern size_t events_read(void *buf, size_t offset, size_t len);
 
 typedef struct
 {
@@ -21,7 +22,8 @@ enum
   FD_STDIN,
   FD_STDOUT,
   FD_STDERR,
-  FD_FB
+  FD_FB,
+  FD_DEV_ENVENTS
 };
 
 // enum
@@ -49,6 +51,7 @@ static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, 0, invalid_read, invalid_write},
     [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
     [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
+    [FD_DEV_ENVENTS] = {"/dev/events", 0, 0, 0, events_read, invalid_write},
 #include "files.h"
 };
 
