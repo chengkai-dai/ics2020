93c4cdab718061947968f3a2703d861a95975e1e
tracer-ics2020 <tracer@njuics.org>
2021-08-25 18:49:50 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 902f209ea12a412f902afffdff677e50     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  18:49:50 up 20 min,  2 users,  load average: 0.10, 0.08, 0.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t serial_write(const void *buf, size_t offset, size_t len);
 
 typedef struct
 {
@@ -46,8 +47,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, 0, invalid_read, invalid_write},
-    [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
-    [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
+    [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -90,29 +91,18 @@ size_t fs_read(int fd, void *buf, size_t len)
 size_t fs_write(int fd, const void *buf, size_t len)
 {
   assert(fd > 0 && fd < file_length);
-  if (fd == 1 || fd == 2)
+
+  if (file_table[fd].write == NULL)
   {
-    char *cbuf = (char *)buf;
-    for (int i = 0; i < len; ++i)
-    {
-      putch(*(cbuf + i));
-    }
+    assert(file_table[fd].open_offset < file_table[fd].size);
+    if (file_table[fd].open_offset + len > file_table[fd].size)
+      len = file_table[fd].size - file_table[fd].open_offset;
+    ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset += len;
     return len;
   }
   else
-  {
-    if (file_table[fd].write == NULL)
-    {
-      assert(file_table[fd].open_offset < file_table[fd].size);
-      if (file_table[fd].open_offset + len > file_table[fd].size)
-        len = file_table[fd].size - file_table[fd].open_offset;
-      ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-      file_table[fd].open_offset += len;
-      return len;
-    }
-    else
-      return file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-  }
+    return file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 }
 size_t fs_lseek(int fd, size_t offset, int whence)
 {
