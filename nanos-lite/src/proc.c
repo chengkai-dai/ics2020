e53e5a12ca24dba8b15227353b6ac602a3a4bb6a
tracer-ics2020 <tracer@njuics.org>
2021-08-28 02:51:27 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 59c1f584c15640f9bd5fb87d125b956d     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  02:51:27 up 1 day,  4:18,  2 users,  load average: 1.39, 1.30, 0.95

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -46,6 +46,7 @@ Context *schedule(Context *prev)
 
   // always select pcb[0] as the new process
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  printf("current->t0 %x\n",current->cp->GPR2);
 
   // then return the new context
   return current->cp;
