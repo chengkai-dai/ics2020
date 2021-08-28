72dba72b38e3acf5f301d7c4a724a68c08f33d31
tracer-ics2020 <tracer@njuics.org>
2021-08-28 03:32:13 -0700

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f9aa27bdb1c2497eab7d3817fcf6f557            Boot ID: 59c1f584c15640f9bd5fb87d125b956d     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  03:32:13 up 1 day,  4:59,  2 users,  load average: 1.56, 1.50, 1.43

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -53,9 +53,9 @@ Context *schedule(Context *prev)
 
 void context_kload(PCB *pcb, void *fun, void *args)
 {
-#define STACK_FRAME RANGE((uint64_t)&pcb->stack,  (uint64_t)(&pcb->stack) + STACK_SIZE)
+#define STACK_FRAME RANGE((uint64_t)&pcb->stack,  (uint64_t)(&pcb->stack) + STACK_SIZE-1)
   printf("&pcb->stack.start %x\n",(uint64_t)&pcb->stack);
-  printf("&pcb->stack.end %x\n",(uint64_t)(&pcb->stack) + STACK_SIZE);
+  printf("&pcb->stack.end %x\n",(uint64_t)(&pcb->stack) + STACK_SIZE-1);
 
   pcb->cp = kcontext(STACK_FRAME, fun, args);
 
